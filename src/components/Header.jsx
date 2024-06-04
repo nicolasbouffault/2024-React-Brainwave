7c30699fec8447cfbedae7934c2f3c53e02adf08
Nicolas Bouffault <nicolas.bouffault@sagittarius.agency>
2024-06-04 21:18:34 +0100

    Markup and styling amends Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
+import Button from "./Button";
 
 const Header = () => {
+  const pathname = useLocation();
   return (
-    <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
@@ -15,13 +18,28 @@ const Header = () => {
               <a
                 href={item.url}
                 key={item.id}
-                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1`}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                  item.onlyMobile ? "lg:hidden" : ""
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1`}
               >
                 {item.title}
               </a>
             ))}
           </div>
         </nav>
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+        >
+          New account
+        </a>
+        <Button className="hidden lg:flex" href="#login">
+          Sign In
+        </Button>
       </div>
     </div>
   );
