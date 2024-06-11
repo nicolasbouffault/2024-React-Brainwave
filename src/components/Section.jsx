748335e390c012ab538631739a962fa05bd1affc
Nicolas Bouffault <nicolas.bouffault@sagittarius.agency>
2024-06-11 21:02:17 +0100

    Complete section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import SectionSvg from "../assets/svg/SectionSvg";
 
 const Section = ({
   className,
   id,
   crosses,
-  crossedOffset,
+  crossesOffset,
   customPaddings,
   children,
 }) => {
   return (
     <div
+      id={id}
       className={`relative ${
         customPaddings ||
         `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${
@@ -18,6 +20,19 @@ const Section = ({
       }`}
     >
       {children}
+      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 md:block lg:left-7.5 xl:left-10"></div>
+      <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 md:block lg:right-7.5 xl:right-10"></div>
+
+      {crosses && (
+        <>
+          <div
+            className={`hidden absolute top-0 left-5 right-7.5 h-0.25 bg-stroke-1 ${
+              crossesOffset && crossesOffset
+            } pointer-events-none lg:block xl:left-10 right-10`}
+          />
+          <SectionSvg crossesOffset={crossesOffset} />
+        </>
+      )}
     </div>
   );
 };
